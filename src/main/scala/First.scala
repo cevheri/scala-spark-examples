2d2f7d6f76a3cd524842409cc56c574bf6b4ef5a
Mehmet Cevheri <cevheribozoglan@gmail.com>
2021-02-18 00:09:39 +0300

    modify  dataframe, nyc opendata >> fire department data

--- src/main/scala/First.scala
+++ src/main/scala/First.scala
@@ -1,19 +1,14 @@
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object First {
-
   def main(args: Array[String]): Unit = {
-
-    val spark: SparkSession = SparkSession.builder.appName("csv-scala-demo").getOrCreate
-    val data: DataFrame = spark.read.option("header","true").csv( "file:///home/cevher/projects/spark-scala-basic-sample/Popular_Baby_Names.csv")
-
+    val spark: SparkSession = SparkSession.builder.appName("csv-scala-demo1").getOrCreate
+    val data: DataFrame = spark.read.option("header", "true").csv("file:///home/cevher/Downloads/Fire_Incident_Dispatch_Data.csv")
     data.printSchema()
 
-    val res: Array[Row] = data.head(10)
-    println(res)
-    res.foreach(f => println(f))
+    data.head(10).foreach(f => println(f))
+
+
 
   }
-
-
 }
